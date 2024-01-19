42e0746e64ecba05a69d86a5841cd69e3fbad217
mariocstweb <mariocstmra@gmail.com>
2024-01-19 15:50:51 +0100

    added Vue Script

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello",
+      message: "Grazie per aver scelto :",
+      img: "https://www.peerbits.com/static/5e207b41ae78e363d0afdfe61c8b749f/189bc/vuejs-devlopment-main.jpg",
     };
   },
 }).mount("#app");
